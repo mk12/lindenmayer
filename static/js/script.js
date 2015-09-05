6e325e140d8f8f8b4d3544764f0f84d810a6c0d1
Mitchell Kember <mk12360@gmail.com>
2015-09-05 12:37:40 -0400

    Add query functions to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,57 @@
+function getQuery() {
+	var query = {};
+	var tokens = window.location.search.substr(1).split('&');
+	for (var i = 0; i < tokens.length; i++) {
+		if (tokens[i] !== undefined && tokens[i] !== '') {
+			var assignment = tokens[i].split('=');
+			var name = decodeURIComponent(assignment[0]);
+			var value = decodeURIComponent(assignment[1]);
+			query[name] = value;
+		}
+	}
+	return query;
+}
+
+function setQuery(query) {
+	var href = window.location.pathname;
+	var i = 0;
+	for (var name in query) {
+		if (query.hasOwnProperty(name) && query[name]) {
+			href += (i === 0) ? '?' : '&';
+			var encName = encodeURIComponent(name);
+			var encValue = encodeURIComponent(query[name]);
+			href += encName + '=' + encValue;
+			i++;
+		}
+	}
+
+	window.location.href = href;
+}
+
 window.onload = function() {
 	var decBtn = document.getElementById('DecDepth');
 	var incBtn = document.getElementById('IncDepth');
 	var thicknessRange = document.getElementById('Thickness');
 	var colorField = document.getElementById('Color');
+	var query = getQuery();
 
-	var update = function() {
-		var path = window.location.pathname;
-		path += '?s=' + thicknessRange.value;
-		var color = colorField.value.trim();
-		if (color !== '') {
-			path += '&c=' + color;
-		}
-		window.location.href = path;
+	var load = function() {
+		thicknessRange.value = query.s ? query.s : '2';
+		colorField.value = query.c ? query.c : '';
 	}
 
+	var update = function() {
+		query.s = thicknessRange.value;
+		query.c = colorField.value;
+		setQuery(query);
+	}
+
+	load();
+
 	thicknessRange.addEventListener('change', update);
+	colorField.addEventListener('keyup', function(e) {
+		if (e.keyCode == 13) {
+			update();
+		}
+	});
 }
